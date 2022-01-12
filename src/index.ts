755e5fe8c9b83f9fde8c5f0775d8092e8a6323b0
aokikoko <wanght@lenovocloud.com>
2022-01-12 22:43:56 +0800

    feat(src/index.ts): del src add new index.ts

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// 整个库的入口文件
+import { AxiosRequestConfig } from './types'
+
+function axios(config: AxiosRequestConfig) {
+  // todo
+}
+
+export default axios
---
+++ src/types/index.ts
@@ -0,0 +1,23 @@
+// 项目中所有公共的类型定义文件
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'OPTIONS'
+  | 'options'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  methods?: Method
+  data?: any
+  params?: any
+}
