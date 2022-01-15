2115f8e8a86b7fda63d56b413a5d9698bac0f151
aokikoko <wanght@lenovocloud.com>
2022-01-16 01:09:41 +0800

    feat: new

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -4,8 +4,48 @@ export function isDate(val: any): val is Date {
   return toString.call(val) === '[object Date]'
 }
 
-export function isObject(val: any): val is Object {
-  // typeof val === 'object'这样不够, 因为val如果是null也是true
-  // 所以需要加上!==null
-  return val !==null && typeof val === 'object'
+// export function isObject (val: any): val is Object {
+//   return val !== null && typeof val === 'object'
+// }
+
+export function isPlainObject(val: any): val is Object {
+  return toString.call(val) === '[object Object]'
+}
+
+export function isFormData(val: any): val is FormData {
+  return typeof val !== 'undefined' && val instanceof FormData
+}
+
+export function isURLSearchParams(val: any): val is URLSearchParams {
+  return typeof val !== 'undefined' && val instanceof URLSearchParams
+}
+
+export function extend<T, U>(to: T, from: U): T & U {
+  for (const key in from) {
+    ;(to as T & U)[key] = from[key] as any
+  }
+  return to as T & U
+}
+
+export function deepMerge(...objs: any[]): any {
+  const result = Object.create(null)
+
+  objs.forEach(obj => {
+    if (obj) {
+      Object.keys(obj).forEach(key => {
+        const val = obj[key]
+        if (isPlainObject(val)) {
+          if (isPlainObject(result[key])) {
+            result[key] = deepMerge(result[key], val)
+          } else {
+            result[key] = deepMerge(val)
+          }
+        } else {
+          result[key] = val
+        }
+      })
+    }
+  })
+
+  return result
 }
